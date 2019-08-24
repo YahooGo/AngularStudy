3d91b8531360846edbea3c4ea4bb09b745eaff2e
wangxiangyu <15724580513@163.com>
2019-08-24 23:31:34 +0800

    超级给力的项目结构架构

--- Angular-App/src/app/app-routing.module.ts
+++ Angular-App/src/app/app-routing.module.ts
@@ -2,7 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user', loadChildren: './modules/user/user.module#UserModule',
+  },
+  {
+    path: 'product', loadChildren: './modules/product/product.module#ProductModule',
+  },
+  {
+    path: 'article', loadChildren: './modules/article/article.module#ArticleModule',
+  },
+  {
+    path: '**', redirectTo: 'user',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
