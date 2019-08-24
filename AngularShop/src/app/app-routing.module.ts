3315ea710e2b63fd790549fdbb116020c4d0e492
wangxiangyu <15724580513@163.com>
2019-08-24 22:14:13 +0800

    嵌套路由，默认路由

--- AngularShop/src/app/app-routing.module.ts
+++ AngularShop/src/app/app-routing.module.ts
@@ -11,28 +11,46 @@ import { SearchComponent } from './components/search/search.component';
  * 2. 配置地址url 和映射组件
  * 3. 此时路由创建完毕在router-outline查看组件
  */
+// const routes: Routes = [
+//   {
+//     path: 'home',
+//     component: HomeComponent,
+//   },
+//   /**
+//    * 动态路由
+//    */
+//   {
+//     path: 'news/:aid',
+//     component: NewsComponent,
+//   },
+//   {
+//     path: 'form',
+//     component: FormComponent,
+//   },
+//   // 匹配不到的路由，全部由** 接管，调转其他路由
+//   {
+//     path: '**',
+//     redirectTo: 'home'
+//   }
+// ];
+
+/**
+ * 嵌套路由
+ */
+
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
-  },
-  /**
-   * 动态路由
-   */
-  {
-    path: 'news/:aid',
-    component: NewsComponent,
-  },
-  {
-    path: 'form',
-    component: FormComponent,
-  },
-  // 匹配不到的路由，全部由** 接管，调转其他路由
-  {
-    path: '**',
-    redirectTo: 'home'
+    path: 'home', component: HomeComponent,
+    children: [
+      { path: 'welcome', component: NewsComponent },
+      /**
+       * 默认路由跳转不到跳转指定路由
+       */
+      { path: '**', redirectTo: 'welcome' }
+    ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
